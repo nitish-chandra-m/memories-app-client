92cc8e1c435a444247f88c90fae68be5bfa93cef
nitish-chandra-m <nitish237@gmail.com>
2021-01-08 16:36:30 +0530

    Updated UI styling

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
+        <Typography variant="h3" align="center">
           Memories
         </Typography>
         <img
